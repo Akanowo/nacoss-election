d9a363bb6686e8d567f089735ee66d81d0b14505
Uko Akanowo <ukoakanowo98@gmail.com>
2021-01-19 19:15:44 +0100

    removed cloudinary in server side

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -12,14 +12,7 @@ const { nanoid } = require('nanoid');
 const fs = require('fs');
 const path = require('path');
 const bcrypt = require('bcrypt');
-const cloudinary = require('cloudinary').v2;
 
-// cloudinary init
-cloudinary.config({
-	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-	api_key: process.env.CLOUDINARY_API_KEY,
-	api_secret: process.env.CLOUDINARY_API_SECRET
-});
 
 const users = [
 	{
